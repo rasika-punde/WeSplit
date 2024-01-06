5cc3f0e6563c36e99889f5c848e40af5afc1d845
Rasika Punde <rasikapunde@gmail.com>
2024-01-06 18:59:54 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Pratik Mistry on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
